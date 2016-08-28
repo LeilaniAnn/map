4a000939542db7baf9f16a5bbd88a8e6bd151385
Leilani Ann Raranga <retrosole@gmail.com>
2016-08-28 12:46:27 -0700

    fix: changed variable names

--- js/lib/app.js
+++ js/lib/app.js
@@ -12,30 +12,31 @@ var ViewModel;
         zoom: 12
     }); // map div
     var meetup = $.getJSON("http://api.meetup.com/groups.json/?zip=94112&radius=12&topic=technology&order=members&key=e4ea6e144b705138235d42a296244&callback=?");
-    var meetupInfo = meetup.then(function(data) {
-
+    var MeetupInfo = meetup.then(function(data) {
         $.each(data.results, function(i, item) {
                 var infowindow = new google.maps.InfoWindow({
                 content: '<div class="panel panel-primary"><h5 class="text-center"><a href="' + item.link + '" target="_blank">' + item.name + '</a></h5></div>' + '<div class="panel-body"><p class="text-center"><img src="' + item.photo_url + '" width="150">' + '<br>' + '<strong>Last active:</strong> ' + item.updated + ' (' + item.members + ' members) ' + '<br>' + item.description + '</p></div>'
                 }); // infowindow content
                 $filterResults.append('<ul class="list-unstyled"><li>' + '<img src="' + item.photo_url + '"width="25px">' + ' ' + item.name + '<br></li></ul>');
-				
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(item.lat, item.lon),
+                this.position=new google.maps.LatLng(item.lat, item.lon);
+                this.marker = new google.maps.Marker({
                     map: map,
+                    position: this.position,
+                    title: name,
                     animation: google.maps.Animation.DROP
                 }); // new marker
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker)
+                this.marker.addListener('click', function() {
+                    infowindow.open(map, this.marker)
                 }); // infowindow
             }) //.each
     }); // meetupInfo
-
+};
     // Load and parse the JSON
     /* Omitted: fetch it from the server however you want */
 
     // // Update view model properties
     // viewModel.pets(parsed.pets);
 
-};
+ //end functon
+
 ko.applyBindings(new ViewModel());
